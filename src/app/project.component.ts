4ae5310651bc47c839f31236815795a37e539d54
Ramesh Kurichi Ponnuswamy <rameshkp@ie.ibm.com>
2017-06-02 16:05:26 +0100

    Defect: Fix project tab to display both rating and admin projects

--- src/app/project.component.ts
+++ src/app/project.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {ProjectService} from './project.service';
 import {Logger} from 'angular2-logger/core';
+import {Observable} from 'rxjs/Rx';
 
 @Component({
   templateUrl: './project.component.html'
@@ -9,9 +10,24 @@ export class ProjectComponent implements OnInit {
   projects = [];
   constructor(private _ps: ProjectService, private _ls: Logger) {}
   ngOnInit() {
-    this._ps.getProjects().subscribe(x => {
-      this._ls.debug('setting projects ' + x);
-      this.projects = x;
+      Observable.forkJoin(
+      this._ps.getProjects(),
+      this._ps.getProjects(true)
+    ).flatMap(x => x)
+        .reduce((x, y) => x.concat(y))
+        .flatMap(x => x)
+        .distinct((p: Project) => p.id)
+        .toArray()
+     .subscribe( x => {
+       this._ls.debug('Projects');
+       this._ls.debug(x);
+       this.projects = x;
     });
   }
+}
+
+interface Project {
+  id: number;
+  admin: string;
+  name: string;
 }
--- src/app/project.service.ts
+++ src/app/project.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from '@angular/core';
 import {Logger} from 'angular2-logger/core';
-import {Http} from '@angular/http';
+import {Http, URLSearchParams} from '@angular/http';
 import {PropertyService} from './properties.service';
 
 @Injectable()
@@ -9,9 +9,15 @@ export class ProjectService {
   constructor(private _logger: Logger, private _http: Http, private _ps: PropertyService) {
   }
 
-  getProjects() {
+  getProjects(admin ?: boolean) {
     const url = this.getBaseUrl();
     this._logger.debug('Get project url ' + url);
+    if (admin) {
+      const params = new URLSearchParams();
+      params.set('admin', 'true');
+      this._logger.debug('Setting url params ' + params);
+      return this._http.get(this.getBaseUrl(), { headers: this._ps.headers, search: params }).map(res => res.json());
+    }
     return this._http.get(this.getBaseUrl(), { headers: this._ps.headers }).map(res => res.json());
   }
 
